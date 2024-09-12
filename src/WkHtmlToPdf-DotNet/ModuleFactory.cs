040c05356347d4d9f009ea0f904c847ca042d8f3
Salih-Can Selvi <mrdariusad@gmail.com>
2024-09-12 07:34:55 +0200

    ✨ feat: add Module for OS X X64, improve OS and arch detection in ModuleFactory

--- src/WkHtmlToPdf-DotNet/ModuleFactory.cs
+++ src/WkHtmlToPdf-DotNet/ModuleFactory.cs
@@ -5,30 +5,48 @@ namespace WkHtmlToPdfDotNet
 {
     internal static class ModuleFactory
     {
+        private static IWkHtmlModule GetWindowsModule(bool isDotNetStandard)
+        {
+            if (isDotNetStandard)
+            {
+                if (RuntimeInformation.ProcessArchitecture == Architecture.X64)
+                {
+                    return new WkHtmlModuleWin64();
+                }
+            }
+            else if (IntPtr.Size == 8) // Is64 bit Arch
+            {
+                return new WkHtmlModuleWin64();
+            }
+
+            return new WkHtmlModuleWin86();
+        }
+
         public static IWkHtmlModule GetModule()
         {
+            var isDotNetStandard = false;
+#if NETSTANDARD2_0
+            isDotNetStandard = true;
+#endif
             try
             {
-#if NETSTANDARD2_0
-                // Windows allows us to probe for either 64 or 86 bit versions
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    if (RuntimeInformation.ProcessArchitecture == Architecture.X64)
-                    {
-#else
-                    if (IntPtr.Size == 8) // Is64 bit Arch
-                    {
-#endif
-                        return new WkHtmlModuleWin64();
-                    }
-                    else
-                    {
-                        return new WkHtmlModuleWin86();
-                    }
-#if NETSTANDARD2_0
+                    return GetWindowsModule(isDotNetStandard);
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    // Return X64 library regardless, since ARM64 needs to run X64 using Rosetta 2
+                    return new WkHtmlModuleOsx64();
                 }
                 else
                 {
+                    if (!isDotNetStandard)
+                    {
+                        return new WkHtmlModule();
+                    }
+
                     switch (RuntimeInformation.ProcessArchitecture)
                     {
                         case Architecture.X64:
@@ -42,22 +60,12 @@ namespace WkHtmlToPdfDotNet
                             return new WkHtmlModule();
                     }
                 }
-#endif
-            }
-            catch (Exception)
-            {
-            }
-
-            // Also try to load it with the method that should use the deps file
-            try
-            {
-                return new WkHtmlModule();
             }
             catch
             {
+                throw new NotSupportedException(
+                    "Unable to load native library. The platform may be missing native dependencies (libjpeg62, etc). Or the current platform is not supported.");
             }
-
-            throw new NotSupportedException("Unable to load native library. The platform may be missing native dependencies (libjpeg62, etc). Or the current platform is not supported.");
         }
     }
 }
